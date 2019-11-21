ebf2e435090c53749b6b1178ca6fe99518dfb8aa
Krishna Marwaha <57726599+krishnamarwaha@users.noreply.github.com>
2019-11-21 17:28:53 +0000

    new change

---
+++ sesne_hat_using_python_krishna.py
@@ -0,0 +1,14 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.clear()
+
+sense.set_pixel(2,2,(0,0,255))
+sense.set_pixel(4,2,(0,0,255))
+sense.set_pixel(3,4,(255,0,0))
+sense.set_pixel(1,5,(255,0,0))
+sense.set_pixel(2,6,(255,0,0))
+sense.set_pixel(3,6,(255,0,0))
+sense.set_pixel(4,6,(255,0,0))
+sense.set_pixel(5,5,(255,0,0))
